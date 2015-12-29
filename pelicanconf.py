5698827569d86d7404aa37e324c932a9ece95a5d
uelei <github@uelei.com>
2015-12-29 15:59:52 -0200

    add cname + theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,4 +32,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
+
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, }
